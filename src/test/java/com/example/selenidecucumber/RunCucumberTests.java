8aefdafa919281e1fa94db15ee25c3965e606708
kovacs.adam <fckcps77@gmail.com>
2021-11-23 19:53:05 +0100

    HTML report

--- src/test/java/com/example/selenidecucumber/RunCucumberTests.java
+++ src/test/java/com/example/selenidecucumber/RunCucumberTests.java
@@ -5,7 +5,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = {"src/test/resources/features"}, glue = {"com.example.selenidecucumber.steps"})
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        features = {"src/test/resources/features"},
+        glue = {"com.example.selenidecucumber.steps"},
+        monochrome = true
+)
 public class RunCucumberTests {
-
 }
